0920c3b9c52fe5de0b4b73c740cd10e6844ec0e6
elamahpla <theamazingness@gmail.com>
2018-11-07 11:09:58 -0600

    Adding Name, Birthday, Server to #finalfantasy

---
+++ src/js/ffLeft.js
@@ -0,0 +1,26 @@
+export const ffLeft = () => {
+  const finalFantasy = document.getElementById('finalfantasy');
+
+  const nameLeft = document.createElement('div');
+  const nameLeftHeading = document.createElement('h1');
+  nameLeftHeading.innerHTML = 'Name:';
+  nameLeft.classList.add('finalfantasy-nameLeft');
+  nameLeft.appendChild(nameLeftHeading);
+  finalFantasy.appendChild(nameLeft);
+
+  const birthdayLeft = document.createElement('div');
+  const birthdayLeftHeading = document.createElement('h3');
+  birthdayLeftHeading.innerHTML = 'Birthday:';
+  birthdayLeft.classList.add('finalfantasy-birthdayLeft');
+  birthdayLeft.appendChild(birthdayLeftHeading);
+  finalFantasy.appendChild(birthdayLeft);
+
+  const serverLeft = document.createElement('div');
+  const serverLeftHeading = document.createElement('h3');
+  serverLeftHeading.innerHTML = 'Server:';
+  serverLeft.classList.add('finalfantasy-serverLeft');
+  serverLeft.appendChild(serverLeftHeading);
+  finalFantasy.appendChild(serverLeft);
+}
+
+ffLeft();
