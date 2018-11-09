db8dafcc837422f052579e8562719bf9b335ed3b
elamahpla <theamazingness@gmail.com>
2018-11-09 15:42:07 -0600

    Styling to the talents section

--- src/js/talents.js
+++ src/js/talents.js
@@ -3,20 +3,30 @@ export const talents = fetch("https://us.api.blizzard.com/wow/character/zul'jin/
   return response.json();
 })
 .then((data) => {
-  console.log(data);
   const output = document.getElementById('wow');
+  const talentsSection = document.createElement('section');
+  const talentsDiv = document.createElement('section');
+  const talentsHeading = document.createElement('h1');
+  talentsHeading.innerHTML = 'Talents:';
+  talentsDiv.classList.add('output-talentHeading');
+  talentsSection.classList.add('output-talents');
   const talentList = document.createElement('ul');
+  talentList.classList.add('output-talentList');
   const talentTest = data.talents[0].talents.map((talent) => {
     return `
       <li>
+        &nbsp;
         <img
           src="https://render-us.worldofwarcraft.com/icons/56/${talent.spell.icon}.jpg"
           alt="${talent.spell.description}"
         >
-        ${talent.spell.name}
+          ${talent.spell.name}
       </li>
-    `
-  })
-  talentList.innerHTML = talentTest.join(" ")
-  output.appendChild(talentList);
+      `
+    })
+  talentList.innerHTML = talentTest.join(" ");
+  talentsSection.appendChild(talentList);
+  talentsDiv.appendChild(talentsHeading);
+  output.appendChild(talentsDiv);
+  output.appendChild(talentsSection);
 })
