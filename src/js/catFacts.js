d2bd12cd9744588623981fa3b70f731946398f6d
elamahpla <theamazingness@gmail.com>
2018-11-24 02:52:00 -0600

    :yikes: I'm just playing around with favoriteCats.js

--- src/js/catFacts.js
+++ src/js/catFacts.js
@@ -20,22 +20,22 @@ export const catFactCall = () => {
       return `
         <li>
           <h1>Information About My Favorite Cat:</h1>
-          <h4>Favorite Cat</h4>: ${info.name} <br />
-          <h4>Favorite Cat Color</h4>: ${info.favoriteColor} <br />
-          <h4>Weight</h4>: ${info.description.weight} <br />
-          <h4>Eye Color</h4>: ${info.description.eyeColor} <br />
-          <h4>Life Span</h4>: ${info.description.lifeSpan} <br ?>
-          <h4>Social & Attention Needs</h4>: ${info.description.socialNeeds} <br />
+          <h4>Favorite Cat:</h4> ${info.name} <br />
+          <h4>Favorite Cat Color:</h4> ${info.favoriteColor} <br />
+          <h4>Weight:</h4> ${info.description.weight} <br />
+          <h4>Eye Color:</h4> ${info.description.eyeColor} <br />
+          <h4>Life Span:</h4> ${info.description.lifeSpan} <br ?>
+          <h4>Social & Attention Needs:</h4> ${info.description.socialNeeds} <br />
 
           <h1>Information About Their Fur:</h1>
-          Type of fur: ${info.coat.length} & ${info.coat.type} <br />
-          Color of fur: ${info.coat.colors} <br />
-          Possible patterns: ${info.coat.pattern} <br />
-          Grooming Needs: ${info.coat.groomingNeeds} <br />
+          <h4>Type of fur:</h4> ${info.coat.length} & ${info.coat.type} <br />
+          <h4>Color of fur:</h4> ${info.coat.colors} <br />
+          <h4>Possible patterns:</h4> ${info.coat.pattern} <br />
+          <h4>Grooming Needs:</h4> ${info.coat.groomingNeeds} <br />
 
           <h1>Living With A Maine Coon:</h1>
-          Living With: ${info.livingWith} <br />
-          History: ${info.history}
+          <h4>Living With:</h4> ${info.livingWith} <br />
+          <h4>History:</h4> ${info.history}
         </li>
       `
     });
