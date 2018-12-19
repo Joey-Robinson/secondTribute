aadb4ccb167b073be5da83fdc65cf09446cccfc3
elamahpla <theamazingness@gmail.com>
2018-12-19 12:23:05 -0600

    Wew. I may have forgotten to push stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,26 @@
 // API
-import catFacts from './api/catFacts';
-import ffCall from './api/finalFantasy';
-import weeklyAffixes from './api/weeklyAffixes';
-import wowCharacter from './api/wowCharacter';
+import catFacts from "./api/catFacts";
+import ffCall from "./api/finalFantasy";
+import weeklyAffixes from "./api/weeklyAffixes";
+import wowCharacter from "./api/wowCharacter";
 // Components
-import {
-  resizeHeaderOnScroll
-} from './components/header';
+import { resizeHeaderOnScroll } from "./components/header";
 // User
-import ffResourcesList from './user/ffResources';
+import ffResourcesList from "./user/ffResources";
+
+// const removeLocationHash = () => {
+//   const noHashURL = window.location.href.replace(/#.*$/, '');
+//   window.history.replaceState('', document.title, noHashURL)
+// }
+
+// window.addEventListener("popstate", (event) => {
+//   event.preventDefault();
+//   removeLocationHash();
+// });
+// window.addEventListener("hashchange", (event) => {
+//   event.preventDefault();
+//   removeLocationHash();
+// });
+// window.addEventListener("load", () => {
+//   removeLocationHash();
+// });
