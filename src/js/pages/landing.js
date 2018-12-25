ddadf7c98ae78c383c4d9ba22d7319563c9f7922
elamahpla <theamazingness@gmail.com>
2018-12-25 01:29:31 -0600

    Adding some images for landing.js

--- src/js/pages/landing.js
+++ src/js/pages/landing.js
@@ -5,9 +5,42 @@ export const something = () => {
     largeImage: '../../src/images/finalfantasybackground.jpg'
   }, {
     thumbnail: '../../images/firecat-tiltshift.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
+    largeImage: '../../src/images/firecat-tiltshift.jpg'
   }, {
     thumbnail: '../../images/firecat.jpg',
+    largeImage: '../../src/images/firecat.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/space.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/finalfantasybackground.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/nagrand.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/zangarmarsh.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/vale.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/kunlai.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/finalfantasybackground.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/finalfantasybackground.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/finalfantasybackground.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
+    largeImage: '../../src/images/finalfantasybackground.jpg'
+  }, {
+    thumbnail: '../../images/space.jpg',
     largeImage: '../../src/images/finalfantasybackground.jpg'
   }, {
     thumbnail: '../../images/space.jpg',
