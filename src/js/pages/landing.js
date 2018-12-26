8faa3fa47c978cc98cb330395654f072b145f02f
elamahpla <theamazingness@gmail.com>
2018-12-26 14:34:20 -0600

    New images with thumbnails to accompany them

--- src/js/pages/landing.js
+++ src/js/pages/landing.js
@@ -1,75 +1,82 @@
-const landingListImages = [
-  {
-    thumbnail: "../../images/finalfantasybackground.jpg",
+const landingListImages = [{
+    thumbnail: "../../src/images/finalfantasybackgroundthumbnail.jpg",
     largeImage: "../../src/images/finalfantasybackground.jpg"
   },
   {
-    thumbnail: "../../images/firecat-tiltshift.jpg",
-    largeImage: "../../src/images/firecat-tiltshift.jpg"
+    thumbnail: "../../src/images/karasangthumbnail.jpg",
+    largeImage: "../../src/images/karasang.jpg"
   },
   {
-    thumbnail: "../../images/firecat.jpg",
+    thumbnail: "../../src/images/wodspiresthumbnail.jpg",
+    largeImage: "../../src/images/wodspires.jpg"
+  },
+  {
+    thumbnail: "../../src/images/firecatthumbnail.jpg",
     largeImage: "../../src/images/firecat.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
-    largeImage: "../../src/images/space.jpg"
+    thumbnail: "../../src/images/wodnagrandthumbnail.jpg",
+    largeImage: "../../src/images/wodnagrand.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
-    largeImage: "../../src/images/finalfantasybackground.jpg"
-  },
-  {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/nagrandthumbnail.jpg",
     largeImage: "../../src/images/nagrand.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/zangarmarshthumbnail.jpg",
     largeImage: "../../src/images/zangarmarsh.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/valethumbnail.jpg",
     largeImage: "../../src/images/vale.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/kunlaithumbnail.jpg",
     largeImage: "../../src/images/kunlai.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/churningmiststhumbnail.jpg",
     largeImage: "../../src/images/churningmists.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/dravanianforelandsthumbnail.jpg",
     largeImage: "../../src/images/dravanianforelands.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/rubyseathumbnail.jpg",
     largeImage: "../../src/images/rubysea.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/seaofcloudsthumbnail.jpg",
     largeImage: "../../src/images/seaofclouds.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
+    thumbnail: "../../src/images/thelochsthumbnail.jpg",
     largeImage: "../../src/images/thelochs.jpg"
   },
   {
-    thumbnail: "../../images/space.jpg",
-    largeImage: "../../src/images/finalfantasybackground.jpg"
+    thumbnail: "../../src/images/legiondalaranthumbnail.jpg",
+    largeImage: "../../src/images/legiondalaran.jpg"
   }
 ];
 
 // Had to add relative pathing from bundle.js
-export const something = () => {
+export const landing = () => {
   landingListImages.sort(() => 0.25 - Math.random());
   const landingList = document.getElementById("landing-list");
   const newLandingList = landingListImages.map((image, index) => {
-    return `<li><img src=${image.largeImage} alt="Landing Area Image ${index +
-      1}"></li>`;
+    return `<li>
+      <a 
+        href=${image.largeImage}
+      >
+        <img 
+          src=${image.thumbnail} 
+          alt="Landing Area Image ${index +1}"
+        >
+      </a>
+      </li>`;
   });
   landingList.innerHTML = newLandingList.join(" ");
 };
 
-something();
+landing();
