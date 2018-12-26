87ab1d2c0058528a1448d9fdd3f85ba40f10b72a
elamahpla <theamazingness@gmail.com>
2018-12-26 12:00:54 -0600

    Few images

--- src/js/pages/landing.js
+++ src/js/pages/landing.js
@@ -1,57 +1,75 @@
+const landingListImages = [
+  {
+    thumbnail: "../../images/finalfantasybackground.jpg",
+    largeImage: "../../src/images/finalfantasybackground.jpg"
+  },
+  {
+    thumbnail: "../../images/firecat-tiltshift.jpg",
+    largeImage: "../../src/images/firecat-tiltshift.jpg"
+  },
+  {
+    thumbnail: "../../images/firecat.jpg",
+    largeImage: "../../src/images/firecat.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/space.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/finalfantasybackground.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/nagrand.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/zangarmarsh.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/vale.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/kunlai.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/churningmists.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/dravanianforelands.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/rubysea.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/seaofclouds.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/thelochs.jpg"
+  },
+  {
+    thumbnail: "../../images/space.jpg",
+    largeImage: "../../src/images/finalfantasybackground.jpg"
+  }
+];
+
 // Had to add relative pathing from bundle.js
 export const something = () => {
-  const landingListImages = [{
-    thumbnail: '../../images/finalfantasybackground.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }, {
-    thumbnail: '../../images/firecat-tiltshift.jpg',
-    largeImage: '../../src/images/firecat-tiltshift.jpg'
-  }, {
-    thumbnail: '../../images/firecat.jpg',
-    largeImage: '../../src/images/firecat.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/space.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/nagrand.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/zangarmarsh.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/vale.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/kunlai.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }, {
-    thumbnail: '../../images/space.jpg',
-    largeImage: '../../src/images/finalfantasybackground.jpg'
-  }];
-
+  landingListImages.sort(() => 0.25 - Math.random());
   const landingList = document.getElementById("landing-list");
   const newLandingList = landingListImages.map((image, index) => {
-    return `<li><img src=${image.largeImage}></li>`
-  })
+    return `<li><img src=${image.largeImage} alt="Landing Area Image ${index +
+      1}"></li>`;
+  });
   landingList.innerHTML = newLandingList.join(" ");
-}
+};
 
 something();
