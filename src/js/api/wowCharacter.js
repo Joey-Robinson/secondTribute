49aa0f3ea2033f641c0a5ca8fdc69b4cbfd8b91c
elamahpla <theamazingness@gmail.com>
2018-12-27 15:10:43 -0600

    Changed layout of header just a tad. Starting on wow

--- src/js/api/wowCharacter.js
+++ src/js/api/wowCharacter.js
@@ -2,19 +2,21 @@ export const wowCharacter = async () => {
   const response = await fetch("https://raider.io/api/v1/characters/profile?region=us&realm=Zul'jin&name=Meerkatz&fields=gear,mythic_plus_scores,mythic_plus_ranks,mythic_plus_recent_runs,mythic_plus_best_runs")
   const data = await response.json();
   console.log(data);
+  const wow = document.querySelector(".wow-primary");
   const name = data.name;
   const spec = data.active_spec_name;
   const druid = data.class;
   const druidInformation = 'https://wow.gamepedia.com/Druid';
-  const plusRankRegion = data.mythic_plus_ranks.class.region;
-  const bestOverall = data.mythic_plus_best_runs;
-  const plusRankRealm = data.mythic_plus_ranks.class.realm;
-  const recentPlus = data.mythic_plus_recent_runs[0];
-  const recentPlusChest = data.mythic_plus_recent_runs[0].num_keystone_upgrades;
-  const recentPlusOne = data.mythic_plus_recent_runs[1];
-  const recentPlusChestOne = data.mythic_plus_recent_runs[1].num_keystone_upgrades;
-  const recentPlusTwo = data.mythic_plus_recent_runs[2];
-  const recentPlusChestTwo = data.mythic_plus_recent_runs[2].num_keystone_upgrade;
+  wow.innerHTML = name;
+  // const plusRankRegion = data.mythic_plus_ranks.class.region;
+  // const bestOverall = data.mythic_plus_best_runs;
+  // const plusRankRealm = data.mythic_plus_ranks.class.realm;
+  // const recentPlus = data.mythic_plus_recent_runs[0];
+  // const recentPlusChest = data.mythic_plus_recent_runs[0].num_keystone_upgrades;
+  // const recentPlusOne = data.mythic_plus_recent_runs[1];
+  // const recentPlusChestOne = data.mythic_plus_recent_runs[1].num_keystone_upgrades;
+  // const recentPlusTwo = data.mythic_plus_recent_runs[2];
+  // const recentPlusChestTwo = data.mythic_plus_recent_runs[2].num_keystone_upgrade;
 
   const changeFavIcon = img => {
     let favIcon = document.querySelector('link[rel="shortcut icon"]');
