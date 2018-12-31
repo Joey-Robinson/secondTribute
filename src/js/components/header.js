4b73947e4833081510dea7d9abe39c82a4059d76
elamahpla <theamazingness@gmail.com>
2018-12-31 12:14:24 -0600

    Cleaning up a bit

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,15 +1,3 @@
-// window.onscroll = function () {
-//   scrollFunction()
-// };
-
-// export function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("header").style.fontSize = "30px";
-//   } else {
-//     document.getElementById("header").style.fontSize = "90px";
-//   }
-// }
-
 window.onscroll = (() => {
   resizeHeaderOnScroll();
 });
