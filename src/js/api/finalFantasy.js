4ec967c05c47c96ddeec6094a499838f2ef396e3
elamahpla <theamazingness@gmail.com>
2019-01-04 16:24:40 -0600

    Adding profession images for final fantasy

--- src/js/api/finalFantasy.js
+++ src/js/api/finalFantasy.js
@@ -5,14 +5,17 @@ import {
 export const ffCall = async () => {
   const response = await fetch("https://xivapi.com/character/10803092");
   const data = await response.json();
-  console.log(data);
+  const ffProfessions = document.querySelector('.finalfantasy-professions');
   const name = data.Character.Name;
   const birthday = data.Character.Nameday;
   const server = data.Character.Server;
   const portrait = data.Character.Portrait;
   const armory = "https://na.finalfantasyxiv.com/lodestone/character/10803092/";
   const freeCompany = "https://na.finalfantasyxiv.com/lodestone/freecompany/9234490298434948752/";
-  console.log(finalFantasyProfessions);
+  const professions = finalFantasyProfessions;
+  for (const professionImage in professions) {
+    ffProfessions.innerHTML += `<img src="${professions[professionImage]}" alt="${professionImage}"/>`
+  }
 }
 
 ffCall();
