f164189a3f35947070f57c19bf0f66f30aa50d2f
elamahpla <theamazingness@gmail.com>
2019-01-10 12:40:25 -0600

    a gif and a webm

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 // API
 // import catFacts from "./api/catFacts";
-// import ffCall from "./api/finalFantasy";
-// import weeklyAffixes from "./api/weeklyAffixes";
+import {
+  ffCall
+} from "./api/finalFantasy";
+
 import meerkats from "./api/meerkats";
 import ristretta from './api/ristretta';
-import finalFantasy from './api/finalFantasy';
 // Components
 import {
   resizeHeaderOnScroll,
@@ -12,11 +13,13 @@ import {
   changeIcon
 } from "./components/header";
 import modalFunction from './components/modal';
+
 // User
-// import ffResourcesList from "./user/ffResources";
 import favoriteCat from './user/favoriteCat';
+
 // Pages
 import landing from './pages/landing';
+
 // const removeLocationHash = () => {
 //   const noHashURL = window.location.href.replace(/#.*$/, '');
 //   window.history.replaceState('', document.title, noHashURL)
