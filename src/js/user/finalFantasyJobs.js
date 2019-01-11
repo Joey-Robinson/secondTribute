ef447d7d37fd34f568beb7636b2c61fdc48b3c88
elamahpla <theamazingness@gmail.com>
2019-01-11 10:51:08 -0600

    uncomment to check prod. This is awful :sob:

--- src/js/user/finalFantasyJobs.js
+++ src/js/user/finalFantasyJobs.js
@@ -1,21 +1,21 @@
 // Prod
-// export const finalFantasyJobs = {
-//   astrologian: '../images/astrologian.png',
-//   whitemage: '../images/whitemage.png',
-//   scholar: '../images/scholar.png',
-//   redmage: '../images/redmage.png',
-//   bard: '../images/bard.png',
-//   summoner: '../images/summoner.png',
-//   warrior: '../images/warrior.png'
-// }
+export const finalFantasyJobs = {
+  astrologian: '../images/astrologian.png',
+  whitemage: '../images/whitemage.png',
+  scholar: '../images/scholar.png',
+  redmage: '../images/redmage.png',
+  bard: '../images/bard.png',
+  summoner: '../images/summoner.png',
+  warrior: '../images/warrior.png'
+}
 
 // Dev
-export const finalFantasyJobs = {
-  astrologian: '../../../dist/images/astrologian.png',
-  whitemage: '../../../dist/images/whitemage.png',
-  scholar: '../../../dist/images/scholar.png',
-  redmage: '../../../dist/images/redmage.png',
-  bard: '../../../dist/images/bard.png',
-  summoner: '../../../dist/images/summoner.png',
-  warrior: '../../../dist/images/warrior.png'
-}
+// export const finalFantasyJobs = {
+//   astrologian: '../../../dist/images/astrologian.png',
+//   whitemage: '../../../dist/images/whitemage.png',
+//   scholar: '../../../dist/images/scholar.png',
+//   redmage: '../../../dist/images/redmage.png',
+//   bard: '../../../dist/images/bard.png',
+//   summoner: '../../../dist/images/summoner.png',
+//   warrior: '../../../dist/images/warrior.png'
+// }
--- src/js/user/finalFantasyMounts.js
+++ src/js/user/finalFantasyMounts.js
@@ -1,37 +1,37 @@
 // Prod
-// export const finalFantasyMounts = {
-//   behemoth: '../images/behemoth.png',
-//   blissful_kamuy: '../images/blissful_kamuy.png',
-//   cloud_mallow: '../images/cloud_mallow.png',
-//   dark_lanner: '../images/dark_lanner.png',
-//   demonic_lanner: '../images/demonic_lanner.png',
-//   fat_chocobo: '../images/fat_chocobo.png',
-//   ixion: '../images/ixion.png',
-//   mikoshi: '../images/mikoshi.png',
-//   mychocobo: '../images/mychocobo.png',
-//   original_fat_chocobo: '../images/original_fat_chocobo.png',
-//   rose_lanner: '../images/rose_lanner.png',
-//   sophic_lanner: '../images/sophic_lanner.png',
-//   twintania: '../images/twintania.png',
-//   tyrannosaur: '../images/tyrannosaur.png',
-//   zu: '../images/zu.png'
-// }
+export const finalFantasyMounts = {
+  behemoth: '../images/behemoth.png',
+  blissful_kamuy: '../images/blissful_kamuy.png',
+  cloud_mallow: '../images/cloud_mallow.png',
+  dark_lanner: '../images/dark_lanner.png',
+  demonic_lanner: '../images/demonic_lanner.png',
+  fat_chocobo: '../images/fat_chocobo.png',
+  ixion: '../images/ixion.png',
+  mikoshi: '../images/mikoshi.png',
+  mychocobo: '../images/mychocobo.png',
+  original_fat_chocobo: '../images/original_fat_chocobo.png',
+  rose_lanner: '../images/rose_lanner.png',
+  sophic_lanner: '../images/sophic_lanner.png',
+  twintania: '../images/twintania.png',
+  tyrannosaur: '../images/tyrannosaur.png',
+  zu: '../images/zu.png'
+}
 
 // Dev
-export const finalFantasyMounts = {
-  behemoth: '../../../dist/images/behemoth.png',
-  blissful_kamuy: '../../../dist/images/blissful_kamuy.png',
-  cloud_mallow: '../../../dist/images/cloud_mallow.png',
-  dark_lanner: '../../../dist/images/dark_lanner.png',
-  demonic_lanner: '../../../dist/images/demonic_lanner.png',
-  fat_chocobo: '../../../dist/images/fat_chocobo.png',
-  ixion: '../../../dist/images/ixion.png',
-  mikoshi: '../../../dist/images/mikoshi.png',
-  mychocobo: '../../../dist/images/mychocobo.png',
-  original_fat_chocobo: '../../../dist/images/original_fat_chocobo.png',
-  rose_lanner: '../../../dist/images/rose_lanner.png',
-  sophic_lanner: '../../../dist/images/sophic_lanner.png',
-  twintania: '../../../dist/images/twintania.png',
-  tyrannosaur: '../../../dist/images/tyrannosaur.png',
-  zu: '../../../dist/images/zu.png'
-}
+// export const finalFantasyMounts = {
+//   behemoth: '../../../dist/images/behemoth.png',
+//   blissful_kamuy: '../../../dist/images/blissful_kamuy.png',
+//   cloud_mallow: '../../../dist/images/cloud_mallow.png',
+//   dark_lanner: '../../../dist/images/dark_lanner.png',
+//   demonic_lanner: '../../../dist/images/demonic_lanner.png',
+//   fat_chocobo: '../../../dist/images/fat_chocobo.png',
+//   ixion: '../../../dist/images/ixion.png',
+//   mikoshi: '../../../dist/images/mikoshi.png',
+//   mychocobo: '../../../dist/images/mychocobo.png',
+//   original_fat_chocobo: '../../../dist/images/original_fat_chocobo.png',
+//   rose_lanner: '../../../dist/images/rose_lanner.png',
+//   sophic_lanner: '../../../dist/images/sophic_lanner.png',
+//   twintania: '../../../dist/images/twintania.png',
+//   tyrannosaur: '../../../dist/images/tyrannosaur.png',
+//   zu: '../../../dist/images/zu.png'
+// }
