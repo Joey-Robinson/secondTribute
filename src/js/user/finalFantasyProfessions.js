cd3147df1160758df9729b0c903545a52c532553
elamahpla <theamazingness@gmail.com>
2019-01-10 20:57:55 -0600

    Changed pathing again, should just make a new branch for this crap

--- src/js/user/finalFantasyProfessions.js
+++ src/js/user/finalFantasyProfessions.js
@@ -1,13 +1,13 @@
 export const finalFantasyProfessions = {
-  alchemist: '../../../dist/images/alchemist.png',
-  armorer: '../../../dist/images/armorer.png',
-  blacksmith: '../../../dist/images/blacksmith.png',
-  carpenter: '../../../dist/images/carpenter.png',
-  culinarian: '../../../dist/images/culinarian.png',
-  goldsmith: '../../../dist/images/goldsmith.png',
-  leatherworking: '../../../dist/images/leatherworking.png',
-  weaver: '../../../dist/images/weaver.png',
-  botanist: '../../../dist/images/botanist.png',
-  fishing: '../../../dist/images/fishing.png',
-  mining: '../../../dist/images/mining.png'
+  alchemist: '../images/alchemist.png',
+  armorer: '../images/armorer.png',
+  blacksmith: '../images/blacksmith.png',
+  carpenter: '../images/carpenter.png',
+  culinarian: '../images/culinarian.png',
+  goldsmith: '../images/goldsmith.png',
+  leatherworking: '../images/leatherworking.png',
+  weaver: '../images/weaver.png',
+  botanist: '../images/botanist.png',
+  fishing: '../images/fishing.png',
+  mining: '../images/mining.png'
 }
