2996d542a40d94f8c4c0a9011868f0676e31a487
elamahpla <theamazingness@gmail.com>
2019-01-15 17:21:24 -0600

    Try this on for size

--- src/js/pages/landing.js
+++ src/js/pages/landing.js
@@ -18,7 +18,6 @@ export const landing = () => {
         <img
           class="lazyload"
           data-src=${image.thumbnail}
-          src=${image.thumbnail}
           alt="Landing Area Image ${index +1}"
         >
       </a>
