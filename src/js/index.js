e34b5fd8428b972e317d62efdb34db0212ac613b
Joey Robinson <theamazingness@gmail.com>
2019-03-05 13:17:48 -0500

    lazy-loading bgimages

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,5 @@ import MeerkatsApiCall from './api/meerkats.api'
 import RistrettaApiCall from './api/ristretta.api'
 
 // Components
+import lazyLoad from './components/lazyLoad'
 import startSlide from './components/slider'
